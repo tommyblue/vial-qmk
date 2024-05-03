a438586a14bc86bd1658cef103cbcfc88a4005df
Tommaso Visconti <tommaso.visconti@gmail.com>
2024-05-04 00:47:25 +0200

    Add charybdis 3x5+3 handwired

---
+++ keyboards/bastardkb/charybdis/3x5_handwired/keymaps/vial/config.h
@@ -0,0 +1,24 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xE3, 0xC2, 0x21, 0xEF, 0xF3, 0xB7, 0x37, 0xA1}
+
+#ifdef VIA_ENABLE
+/* VIA configuration. */
+#    define DYNAMIC_KEYMAP_LAYER_COUNT 7
+#endif // VIA_ENABLE
+
+#ifndef __arm__
+/* Disable unused features. */
+#    define NO_ACTION_ONESHOT
+#endif // __arm__
+
+/* Charybdis-specific features. */
+
+#ifdef POINTING_DEVICE_ENABLE
+// Automatically enable the pointer layer when moving the trackball.  See also:
+// - `CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS`
+// - `CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD`
+#define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
+#endif // POINTING_DEVICE_ENABLE
