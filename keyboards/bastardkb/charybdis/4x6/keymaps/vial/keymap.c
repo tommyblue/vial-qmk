2ba50c2ea6defcc580ff9cb9ad13d885f0712cb2
Tommaso Visconti <tommaso.visconti@gmail.com>
2024-11-11 17:37:22 +0100

    charybdis:4x6: update trackball

--- keyboards/bastardkb/charybdis/4x6/keymaps/vial/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/vial/keymap.c
@@ -53,10 +53,14 @@ static uint16_t auto_pointer_layer_timer = 0;
 #define TO_G TO(_LAYER_GAMING)
 
 #ifndef POINTING_DEVICE_ENABLE
-#    define DRGSCRL KC_NO
-#    define DPI_MOD KC_NO
-#    define S_D_MOD KC_NO
-#    define SNIPING KC_NO
+#        define DPI_MOD POINTER_DEFAULT_DPI_FORWARD
+#        define DPI_RMOD POINTER_DEFAULT_DPI_REVERSE
+#        define S_D_MOD POINTER_SNIPING_DPI_FORWARD
+#        define S_D_RMOD POINTER_SNIPING_DPI_REVERSE
+#        define SNIPING SNIPING_MODE
+#        define SNP_TOG SNIPING_MODE_TOGGLE
+#        define DRGSCRL DRAGSCROLL_MODE
+#        define DRG_TOG DRAGSCROLL_MODE_TOGGLE
 #endif // !POINTING_DEVICE_ENABLE
 
 // #define CHARYBDIS_MINIMUM_DEFAULT_DPI 800
@@ -225,8 +229,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
 }
 
 #define LAYOUT_LAYER_COLEMAK                                                                                            \
-      KC_ESC,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_DEL,
-          \
+      KC_ESC,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_DEL,     \
       KC_TAB,     KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,       KC_J,    KC_L,    KC_U,    KC_Y,    KC_MINS, KC_BSPC, \
       KC_LSFT,    KC_A,    KC_R,    KC_S,    KC_T,    KC_D,       KC_H,    KC_N,    KC_E,    KC_I,    KC_O, KC_RSFT,    \
       KC_LCTL,    PT_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_K,    KC_M, KC_COMM,  KC_DOT, PT_SLSH, KC_RCTL,    \
@@ -267,11 +270,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
                                              _______,  _______,    _______
 
 #define LAYOUT_LAYER_POINTER                                                                                          \
-      XXXXXXX, QK_BOOT, EE_CLR,  XXXXXXX, TO_B,     TO_G,        KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU,\
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DPI_RMOD, S_D_RMOD,    S_D_MOD, DPI_MOD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
-      XXXXXXX, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT,  XXXXXXX,     XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, XXXXXXX,\
-      XXXXXXX, _______, DRGSCRL, SNIPING, XXXXXXX,  XXXXXXX,     XXXXXXX, XXXXXXX, SNIPING, DRGSCRL, _______, XXXXXXX,\
-                                 KC_BTN2, KC_BTN1,  KC_BTN3,     KC_BTN3, KC_BTN1,                                    \
+      XXXXXXX, QK_BOOT, EE_CLR,  XXXXXXX, TO_B,     TO_G,        KC_MPRV,  KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU,\
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, S_D_RMOD, S_D_MOD,     DPI_RMOD, DPI_MOD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
+      XXXXXXX, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT,  XXXXXXX,     XXXXXXX,  KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, XXXXXXX,\
+      XXXXXXX, _______, DRGSCRL, SNIPING, XXXXXXX,  XXXXXXX,     XXXXXXX,  XXXXXXX, SNIPING, DRGSCRL, _______, XXXXXXX,\
+                                 KC_BTN2, KC_BTN1,  KC_BTN3,     KC_BTN3,  KC_BTN1,                                    \
                                           XXXXXXX,  KC_BTN2,     KC_BTN2
 
 #define LAYOUT_wrapper(...) LAYOUT(__VA_ARGS__)
