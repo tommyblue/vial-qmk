ff01e2e999d1abedbc8b2b0823c6ff56aa51f8cf
Tommaso Visconti <tommaso.visconti@gmail.com>
2025-01-16 09:25:38 +0100

    Add charybdis 3x6 with amoeba keys

--- keyboards/bastardkb/charybdis/3x6_handwired/config.h
+++ keyboards/bastardkb/charybdis/3x6_handwired/config.h
@@ -21,8 +21,6 @@
 /* Trackball angle adjustment. */
 #define ROTATIONAL_TRANSFORM_ANGLE -25
 
-#define PMW33XX_LIFTOFF_DISTANCE 0x08
-
 // https://docs.qmk.fm/tap_hold
 #define TAPPING_TERM 250
 #define PERMISSIVE_HOLD
